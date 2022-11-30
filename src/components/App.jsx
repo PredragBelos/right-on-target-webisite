b63d81f55cb0373cb58346da49586c1779ae7631
PredragBelos <predragbelos@gmail.com>
2022-11-30 20:57:08 +0100

    Inser font files and prepare font scss file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <div className='App'>
       <BrowserRouter>
         <Routes>
-          
+          {/* <Route path='/' element={<Box />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
