fc30306e5318b3561ed17c8f1262e0885db8eb2b
PredragBelos <predragbelos@gmail.com>
2023-01-03 11:13:02 +0100

    Create task for creating news component by react-cli

---
+++ src/components/newsComponent/News/news_1.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const News__1 = () => {
+
+    // STATE ----------------------------------------------------------------------
+
+    // LIFECICLE ------------------------------------------------------------------
+
+    // FUNCTIONS ------------------------------------------------------------------ 
+
+    // RENDER ---------------------------------------------------------------------
+    return (
+        <section className='news__1'>
+            <div className='wrap container-section page-view news__1-container'>
+
+            </div>
+        </section>
+    )
+}
