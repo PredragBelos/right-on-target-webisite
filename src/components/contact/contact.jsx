de317d9b03d33bd01b4089907f0d99b05cab74a6
PredragBelos <predragbelos@gmail.com>
2023-01-04 17:41:48 +0100

    Change news_1 page style

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Header } from '../header/Header';
 import { Footer } from '../footer/footer';
 import emailjs from '@emailjs/browser';
