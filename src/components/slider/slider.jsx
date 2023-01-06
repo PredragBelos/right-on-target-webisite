74939da1b7f7508c21c018c6ca90bdee28449dd3
PredragBelos <predragbelos@gmail.com>
2023-01-06 15:33:49 +0100

    Set slider links for previewing news

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { changeSliderData } from '../../js/functions/sliderFunctions';
 
 export const Slider = ({ slider_data, slider_interval }) => {
@@ -7,17 +8,18 @@ export const Slider = ({ slider_data, slider_interval }) => {
     const [slider_title, set_slider_title] = useState(slider_data[0].title);
     const [slider_text, set_slider_text] = useState(slider_data[0].text);
     const [slider_img, set_slider_img] = useState(slider_data[0].image);
+    const [slider_link, set_slider_link] = useState(slider_data[0].news_id);
 
     // LIFECICLE ------------------------------------------------------------------
 
     // Start slider after page loading
     useEffect(() => {
         // Set first ciklus
-        changeSliderData(slider_data, set_slider_title, set_slider_text, set_slider_img);
+        changeSliderData(slider_data, set_slider_title, set_slider_text, set_slider_img, set_slider_link);
 
         // Cretae infinite loop for slider
         const interval = setInterval(
-            () => { changeSliderData(slider_data, set_slider_title, set_slider_text, set_slider_img) }, slider_interval
+            () => { changeSliderData(slider_data, set_slider_title, set_slider_text, set_slider_img, set_slider_link) }, slider_interval
         );
 
         return () => clearInterval(interval);
@@ -37,7 +39,7 @@ export const Slider = ({ slider_data, slider_interval }) => {
                         <div className='slider__content'>
                             <h4 className='slider__title' id='slider-title'>{slider_title}</h4>
                             <p className='slider__text' id='slider-text'>{slider_text}</p>
-                            <button className='btn btn--slider slider__btn'>Više</button>
+                            <Link className='btn btn--slider slider__btn' to={`/news/${slider_link}`}>Više</Link>
                         </div>
                     </div>
                 </div>
--- src/js/functions/sliderFunctions.js
+++ src/js/functions/sliderFunctions.js
@@ -2,12 +2,13 @@
 
 // Function for changing slider data
 let data_index = 0;
-export const changeSliderData = (slider_data, set_title, set_text, set_img) => {
+export const changeSliderData = (slider_data, set_title, set_text, set_img, set_link) => {
 
     // Setting slider data
     set_title(slider_data[data_index].title);
     set_text(slider_data[data_index].text);
     set_img(slider_data[data_index].image);
+    set_link(slider_data[data_index].news_id);
 
     // Setting infinite slider loop
     (data_index > slider_data.length - 2) ? data_index = 0 : data_index++;
